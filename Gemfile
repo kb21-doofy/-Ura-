2b2ee3a79979a5adfba92e4dae96f87fbf8b4121
kb21-doofy <ujifc26kyouhei0201@ezweb.ne.jp>
2024-07-29 13:12:58 +0000

    CSS

--- Gemfile
+++ Gemfile
@@ -3,8 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.4"
 
+
 gem "rails",           "7.0.4.3"
 gem "bootstrap-sass",  "3.4.1"
+
+
 gem "sassc-rails",     "2.1.2"
 gem "sprockets-rails", "3.4.2"
 gem "importmap-rails", "1.1.5"
@@ -37,5 +40,8 @@ group :test do
   gem "guard-minitest",           "2.4.6"
 end
 
+group :production do
+  gem "pg", "1.3.5"
+end
 # Windows ではタイムゾーン情報用の tzinfo-data gem を含める必要があります
 #gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
-  # GET /users/new
+  # GET /users/newz
   def new
     @user = User.new
   end
@@ -21,40 +22,22 @@ class UsersController < ApplicationController
 
   # POST /users or /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new(user_params)   # 実装は終わっていないことに注意!
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the -Ura-"
+    else
+      render 'new', status: :unprocessable_entity
     end
   end
+
+   
 
   # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -65,6 +48,8 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-end
+
+  end
+
